027e6e0808dd4aee07510d870a47d7a1912af9e4
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-03-08 01:02:51 +0200

    Context DbSets added.

--- FoodDeliveryApp.Infrastructure/Data/ApplicationDbContext.cs
+++ FoodDeliveryApp.Infrastructure/Data/ApplicationDbContext.cs
@@ -51,5 +51,23 @@ namespace FoodDeliveryApp.Infrastructure.Data
 
             base.OnModelCreating(builder);
         }
+
+        public DbSet<AddOn> AddOns { get; set; }
+        public DbSet<BlogArticle> BlogArticles { get; set; }
+        public DbSet<BlogCategory> BlogCategories { get; set; }
+        public DbSet<BlogComment> BlogComments { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Coupon> Coupons { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<ItemAddOn> ItemsAddOns { get; set; }
+        public DbSet<ItemCategory> ItemCategories { get; set; }
+        public DbSet<ItemReview> ItemReviews { get; set; }
+        public DbSet<ItemSpicyCategory> ItemSpicyCategories { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<RestaurantCategory> RestaurantCategories { get; set; }
     }
 }
