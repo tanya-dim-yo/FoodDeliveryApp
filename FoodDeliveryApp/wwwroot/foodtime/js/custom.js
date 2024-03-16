b1928ade499ee338f9823a43bdc7f40e4866d337
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-03-16 23:24:05 +0200

    Header customized.

--- FoodDeliveryApp/wwwroot/foodtime/js/custom.js
+++ FoodDeliveryApp/wwwroot/foodtime/js/custom.js
@@ -879,10 +879,10 @@ var foodtime = function(){
 	//}
 	
 	/* Handle Support ============ */
-	var handleSupport = function(){
-		var support = '<script id="DZScript" src="https://dzassets.s3.amazonaws.com/w3-global.js"></script>';
-		jQuery('body').append(support);
-	}
+	//var handleSupport = function(){
+	//	var support = '<script id="DZScript" src="https://dzassets.s3.amazonaws.com/w3-global.js"></script>';
+	//	jQuery('body').append(support);
+	//}
 
 	/* Function ============ */
 	return {
