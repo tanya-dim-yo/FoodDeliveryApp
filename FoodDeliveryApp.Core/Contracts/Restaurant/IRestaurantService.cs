d01505cd1680cf9234685cd64851f5920e102299
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-03-19 00:10:10 +0200

    IRestaurantService added.

---
+++ FoodDeliveryApp.Core/Contracts/Restaurant/IRestaurantService.cs
@@ -0,0 +1,12 @@
+﻿using FoodDeliveryApp.Core.Models.Restaurant;
+
+namespace FoodDeliveryApp.Core.Contracts.Restaurant
+{
+	public interface IRestaurantService
+	{
+		Task AddAsync(RestaurantDetailViewModel model);
+		Task EditAsync(RestaurantDetailViewModel model);
+		Task<IEnumerable<RestaurantViewModel>> GetAllAsync();
+		Task<RestaurantDetailViewModel?> GetByIdAsync(int id);
+	}
+}
