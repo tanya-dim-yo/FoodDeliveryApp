947c4c61679133211714c732370fb6640692ec41
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-03-19 00:09:52 +0200

    RestaurantDetailViewModel added.

---
+++ FoodDeliveryApp.Core/Models/Restaurant/RestaurantDetailViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodDeliveryApp.Core.Models.Restaurant
+{
+	public class RestaurantDetailViewModel
+	{
+	}
+}
