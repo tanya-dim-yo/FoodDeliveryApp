c1f41e4ee84ca9d23d5448d7f6f8b5fc3364d48b
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-03-19 00:02:53 +0200

    RestaurantViewModel added.

--- FoodDeliveryApp.Core/Models/Restaurant/RestaurantViewModel.cs
+++ FoodDeliveryApp.Core/Models/Restaurant/RestaurantViewModel.cs
@@ -1,6 +1,21 @@
 ﻿namespace FoodDeliveryApp.Core.Models.Restaurant
 {
-    public class RestaurantViewModel
+	public class RestaurantViewModel
     {
-    }
+		public int Id { get; set; }
+
+		public string Title { get; set; } = string.Empty;
+
+		public decimal ServiceFee { get; set; }
+
+		public string DeliveryTime { get; set; } = string.Empty;
+
+		public string BackgroundImage { get; set; } = string.Empty;
+
+		public double AverageRating { get; private set; }
+
+		public int TotalReviews { get; private set; }
+
+		public string RestaurantCategory { get; set; } = string.Empty;
+	}
 }
