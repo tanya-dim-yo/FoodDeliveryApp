47171c1a472983863c2662b2992163dc91a415b2
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-03-22 01:40:26 +0200

    Detailed restaurant view model added.

--- FoodDeliveryApp.Core/Models/Restaurant/RestaurantDetailViewModel.cs
+++ FoodDeliveryApp.Core/Models/Restaurant/RestaurantDetailViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FoodDeliveryApp.Core.Models.Item;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,24 @@ namespace FoodDeliveryApp.Core.Models.Restaurant
 {
 	public class RestaurantDetailViewModel
 	{
+		public int Id { get; set; }
+
+		public string Title { get; set; } = string.Empty;
+
+		public decimal ServiceFee { get; set; }
+
+		public string DeliveryTime { get; set; } = string.Empty;
+
+		public string BackgroundImage { get; set; } = string.Empty;
+
+		public double AverageRating { get; set; }
+
+		public int TotalReviews { get; set; }
+
+		public string RestaurantCategory { get; set; } = string.Empty;
+
+		public double AverageRatingPercent => (AverageRating / 5) * 100;
+
+		public virtual IEnumerable<ItemViewModel> Items { get; set; } = new List<ItemViewModel>();
 	}
 }
