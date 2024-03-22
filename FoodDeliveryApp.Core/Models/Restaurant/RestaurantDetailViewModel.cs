09b8a5ba71de3ffe532751bbcdd1452497837b0a
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-03-22 04:44:32 +0200

    Restaurant-menu combined view model added.

--- FoodDeliveryApp.Core/Models/Restaurant/RestaurantDetailViewModel.cs
+++ FoodDeliveryApp.Core/Models/Restaurant/RestaurantDetailViewModel.cs
@@ -1,32 +1,11 @@
 ﻿using FoodDeliveryApp.Core.Models.Item;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FoodDeliveryApp.Core.Models.Restaurant
 {
 	public class RestaurantDetailViewModel
 	{
-		public int Id { get; set; }
+		public RestaurantViewModel Restaurant { get; set; } = new RestaurantViewModel();
 
-		public string Title { get; set; } = string.Empty;
-
-		public decimal ServiceFee { get; set; }
-
-		public string DeliveryTime { get; set; } = string.Empty;
-
-		public string BackgroundImage { get; set; } = string.Empty;
-
-		public double AverageRating { get; set; }
-
-		public int TotalReviews { get; set; }
-
-		public string RestaurantCategory { get; set; } = string.Empty;
-
-		public double AverageRatingPercent => (AverageRating / 5) * 100;
-
-		public virtual IEnumerable<ItemViewModel> Items { get; set; } = new List<ItemViewModel>();
+		public IEnumerable<ItemViewModel> Items { get; set; } = new List<ItemViewModel>();
 	}
 }
