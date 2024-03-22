2f5062022ea894f6f827f8c55622a2c5723bb6ea
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-03-22 02:22:16 +0200

    Item view model added.

--- FoodDeliveryApp.Core/Models/Item/ItemViewModel.cs
+++ FoodDeliveryApp.Core/Models/Item/ItemViewModel.cs
@@ -1,6 +1,23 @@
 ﻿namespace FoodDeliveryApp.Core.Models.Item
 {
-    public class ItemViewModel
+	public class ItemViewModel
     {
-    }
+		public int Id { get; set; }
+
+		public string Title { get; set; } = string.Empty;
+
+		public string Description { get; set; } = string.Empty;
+
+		public decimal Price { get; set; }
+
+		public double AverageRating { get; set; }
+
+		public int TotalReviews { get; set; }
+
+		public string Image { get; set; } = string.Empty;
+
+		public string ItemCategory { get; set; } = string.Empty;
+
+		public double AverageRatingPercent => (AverageRating / 5) * 100;
+	}
 }
