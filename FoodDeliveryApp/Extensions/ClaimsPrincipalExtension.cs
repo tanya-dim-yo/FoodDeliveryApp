4a304ba42828ad7ec78cfe1621b37086480a996c
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-03-25 01:24:08 +0200

    ClaimsPrincipalExtension added.

---
+++ FoodDeliveryApp/Extensions/ClaimsPrincipalExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace FoodDeliveryApp.Extensions
+{
+	public static class ClaimsPrincipalExtension
+	{
+		public static string GetUserId(this ClaimsPrincipal user)
+		{
+			return user.FindFirstValue(ClaimTypes.NameIdentifier);
+		}
+	}
+}
