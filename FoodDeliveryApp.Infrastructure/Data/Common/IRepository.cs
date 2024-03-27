8a6d28e25aaecabb559efb829472758e78b2adf2
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-03-27 09:49:25 +0200

    Repository updated with Delete and GetById method.

--- FoodDeliveryApp.Infrastructure/Data/Common/IRepository.cs
+++ FoodDeliveryApp.Infrastructure/Data/Common/IRepository.cs
@@ -8,6 +8,10 @@
 
 		Task AddAsync<T>(T entity) where T : class;
 
+		Task<T?> GetByIdAsync<T>(object id) where T : class;
+
+		Task DeleteAsync<T>(object id) where T : class;
+
 		Task<int> SaveChangesAsync();
 	}
 }
