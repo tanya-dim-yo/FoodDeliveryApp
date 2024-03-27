7d2369a56e7d524b22273d3d9c6ac6071a3fb26d
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-03-27 13:44:44 +0200

    Restaurant search result model added.

---
+++ FoodDeliveryApp.Core/Models/Restaurant/RestaurantSearchResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodDeliveryApp.Core.Models.Restaurant
+{
+	public class RestaurantSearchResult
+	{
+		public string SanitizedKeyword { get; set; } = string.Empty;
+		public virtual IEnumerable<RestaurantViewModel> Results { get; set; } = new List<RestaurantViewModel>();
+	}
+}
