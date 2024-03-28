78f23505a2f737e17443648a4171c38be113e339
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-03-28 10:53:02 +0200

    CityConfiguration class added.

---
+++ FoodDeliveryApp.Infrastructure/Data/Configuration/CityConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using FoodDeliveryApp.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FoodDeliveryApp.Infrastructure.Data.Configuration
+{
+	public class CityConfiguration : IEntityTypeConfiguration<City>
+	{
+		private City[] initialCities = new City[]
+	    {
+			new City() { Id = 1, Name = "София" },
+			new City() { Id = 2, Name = "Пловдив" },
+			new City() { Id = 3, Name = "Варна" }
+	    };
+
+		public void Configure(EntityTypeBuilder<City> builder)
+		{
+			builder.HasData(initialCities);
+		}
+	}
+}
