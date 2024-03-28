faa9ac619d3340991ab2d348c4573b8781bdacfb
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-03-28 10:59:21 +0200

    Migration made to the database.

---
+++ FoodDeliveryApp.Infrastructure/Migrations/20240328085800_AddInitialCities.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodDeliveryApp.Infrastructure.Migrations
+{
+    public partial class AddInitialCities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Restaurants");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CityId",
+                table: "Restaurants",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "София" });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "Пловдив" });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 3, "Варна" });
+
+            migrationBuilder.UpdateData(
+                table: "Restaurants",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CityId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Restaurants",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CityId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Restaurants",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CityId",
+                value: 1);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Restaurants_CityId",
+                table: "Restaurants",
+                column: "CityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Restaurants_Cities_CityId",
+                table: "Restaurants",
+                column: "CityId",
+                principalTable: "Cities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Restaurants_Cities_CityId",
+                table: "Restaurants");
+
+            migrationBuilder.DropTable(
+                name: "Cities");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Restaurants_CityId",
+                table: "Restaurants");
+
+            migrationBuilder.DropColumn(
+                name: "CityId",
+                table: "Restaurants");
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Restaurants",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Restaurants",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "City",
+                value: "Sofia");
+
+            migrationBuilder.UpdateData(
+                table: "Restaurants",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "City",
+                value: "Sofia");
+
+            migrationBuilder.UpdateData(
+                table: "Restaurants",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "City",
+                value: "Sofia");
+        }
+    }
+}
