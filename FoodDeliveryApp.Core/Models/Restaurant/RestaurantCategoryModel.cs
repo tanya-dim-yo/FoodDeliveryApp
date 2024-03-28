2895791a02e6a2613bc3aa5f806ddb1b4318425a
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-03-28 10:10:33 +0200

    RestaurantCategoryServiceModel renamed to RestaurantCategoryModel.

---
+++ FoodDeliveryApp.Core/Models/Restaurant/RestaurantCategoryModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FoodDeliveryApp.Core.Models.Restaurant
+{
+	public class RestaurantCategoryModel
+	{
+		public int Id { get; set; }
+
+		public string Title { get; set; } = string.Empty;
+	}
+}
