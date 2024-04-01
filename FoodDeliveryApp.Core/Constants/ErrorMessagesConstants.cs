b78e0c0df5def16fcfea1324fa929e56084bee7e
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-01 16:00:41 +0300

    ErrorMessagesConstants class added.

---
+++ FoodDeliveryApp.Core/Constants/ErrorMessagesConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace FoodDeliveryApp.Core.Constants
+{
+	public static class ErrorMessagesConstants
+	{
+		public static class RestaurantErrorMessagesConstants
+		{
+			public const string InvalidCategoryErrorMessage = "Категорията не съществува.";
+			public const string InvalidRestaurantErrorMessage = "Ресторантът не съществува.";
+		}
+	}
+}
