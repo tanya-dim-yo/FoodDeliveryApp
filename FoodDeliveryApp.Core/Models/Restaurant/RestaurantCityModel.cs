251aec815ac711330d39da6b04d5c62c584175ae
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-03 04:25:54 +0300

    RestaurantCityModel added.

---
+++ FoodDeliveryApp.Core/Models/Restaurant/RestaurantCityModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FoodDeliveryApp.Core.Models.Restaurant
+{
+	public class RestaurantCityModel
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; } = string.Empty;
+	}
+}
