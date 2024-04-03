7a2bcecc6a233ba44fadca9eab642d4bc90facb0
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-04 02:01:00 +0300

    ProductDetailsViewModel added.

---
+++ FoodDeliveryApp.Core/Models/Product/ProductDetailsViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace FoodDeliveryApp.Core.Models.Product
+{
+	public class ProductDetailsViewModel
+	{
+		public int Id { get; set; }
+
+		public string Title { get; set; } = string.Empty;
+
+		public string Description { get; set; } = string.Empty;
+
+		public decimal Price { get; set; }
+
+		public double AverageRating { get; set; }
+
+		public int TotalReviews { get; set; }
+
+		public bool IsFavourite { get; set; }
+
+		public bool IsVeggie { get; set; }
+
+		public string ImageURL { get; set; } = string.Empty;
+
+		public string ItemCategory { get; set; } = string.Empty;
+
+		public string SpicyCategory { get; set; } = string.Empty;
+
+		public double AverageRatingPercent => (AverageRating / 5) * 100;
+	}
+}
