c097c2c4aca479dfd798bba8ed1784125b521c2c
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-04 01:15:31 +0300

    Migration ImageURLRenamed added.

---
+++ FoodDeliveryApp.Infrastructure/Migrations/20240403221355_ImageURLRenamed.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodDeliveryApp.Infrastructure.Migrations
+{
+    public partial class ImageURLRenamed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Image",
+                table: "Items",
+                newName: "ImageURL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageURL",
+                table: "Items",
+                newName: "Image");
+        }
+    }
+}
