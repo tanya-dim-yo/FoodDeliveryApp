ebe7365aa3a46cee9b752fd2ea8e7dad54f452a5
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-03 20:19:02 +0300

    Menu view updated.

---
+++ FoodDeliveryApp.Core/Contracts/IProductService.cs
@@ -0,0 +1,6 @@
+﻿namespace FoodDeliveryApp.Core.Contracts
+{
+	public class IProductService
+	{
+	}
+}
---
+++ FoodDeliveryApp/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodDeliveryApp.Controllers
+{
+    public class ProductController : BaseController
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
