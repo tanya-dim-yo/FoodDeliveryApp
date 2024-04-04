88864f01cf63c8af486d1ef149c21ab5387ef592
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-04 05:04:30 +0300

    JavaScript added for favourite product status.

---
+++ FoodDeliveryApp/wwwroot/foodtime/js/favorite.js
@@ -0,0 +1,32 @@
+﻿// favorite.js
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('favoriteToggle').addEventListener('click', function () {
+        toggleFavourite();
+    });
+});
+
+function toggleFavourite() {
+    var productId = '@Model.Id';
+    var isFavourite = '@Model.IsFavourite';
+
+    $.ajax({
+        url: '/Product/Favourite',
+        type: 'POST',
+        data: { productId: productId, isFavourite: !isFavourite },
+        success: function (response) {
+            if (response.success) {
+                var favoriteIcon = document.getElementById('favoriteIcon');
+                if (response.isFavorite) {
+                    favoriteIcon.querySelector('path').setAttribute('fill', '#f50505'); // Set color to red
+                } else {
+                    favoriteIcon.querySelector('path').setAttribute('fill', '#000000'); // Set color to black
+                }
+            } else {
+                console.error('Error toggling favorite status:', response.error);
+            }
+        },
+        error: function (xhr, status, error) {
+            console.error('AJAX error:', error);
+        }
+    });
+}
