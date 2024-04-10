b89e686b3df13b2406e2596a7982c497cd0d4342
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-10 09:49:25 +0300

    ProductFavouriteRequestModel added.

---
+++ FoodDeliveryApp.Core/Models/Product/ProductFavouriteRequestModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FoodDeliveryApp.Core.Models.Product
+{
+	public class ProductFavouriteRequestModel
+	{
+		public int ProductId { get; set; }
+		public bool IsFavourite { get; set; }
+	}
+}
