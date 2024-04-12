26051d13693b3ce07e08cabfcb611a976fbe7c84
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-13 01:58:25 +0300

    Properties for pagination added.

--- FoodDeliveryApp.Core/Models/Restaurant/RestaurantsWithCategoriesViewModel.cs
+++ FoodDeliveryApp.Core/Models/Restaurant/RestaurantsWithCategoriesViewModel.cs
@@ -1,7 +1,10 @@
 ﻿namespace FoodDeliveryApp.Core.Models.Restaurant
 {
-	public class RestaurantWithCategoriesViewModel
+	public class RestaurantsWithCategoriesViewModel
 	{
+		public int RestaurantsPerPage { get; } = 6;
+		public int CurrentPage { get; init; } = 1;
+		public int TotalRestaurantsCount { get; set; }
 		public IEnumerable<string> CategoryNames { get; set; } = new List<string>();
 		public IEnumerable<int> CategoryIds { get; set; } = new List<int>();
 		public IEnumerable<RestaurantViewModel> RestaurantViewModels { get; set; } = new List<RestaurantViewModel>();
