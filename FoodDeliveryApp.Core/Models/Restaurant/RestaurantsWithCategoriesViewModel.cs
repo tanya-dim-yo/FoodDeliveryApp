e2205568ae3a0ee0022398c6ec3e0bdc6dbcaf37
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-13 03:42:10 +0300

    Init setter removed for CurrentPage property.

--- FoodDeliveryApp.Core/Models/Restaurant/RestaurantsWithCategoriesViewModel.cs
+++ FoodDeliveryApp.Core/Models/Restaurant/RestaurantsWithCategoriesViewModel.cs
@@ -1,12 +1,13 @@
 ﻿namespace FoodDeliveryApp.Core.Models.Restaurant
 {
-	public class RestaurantsWithCategoriesViewModel
-	{
-		public int RestaurantsPerPage { get; } = 9;
-		public int CurrentPage { get; init; } = 1;
-		public int TotalRestaurantsCount { get; set; }
-		public IEnumerable<string> CategoryNames { get; set; } = new List<string>();
-		public IEnumerable<int> CategoryIds { get; set; } = new List<int>();
-		public IEnumerable<RestaurantViewModel> RestaurantViewModels { get; set; } = new List<RestaurantViewModel>();
-	}
+    public class RestaurantsWithCategoriesViewModel
+    {
+        public int RestaurantsPerPage { get; } = 2;
+        public int CurrentPage { get; set; }
+        public int TotalRestaurantsCount { get; set; }
+        public IEnumerable<string> CategoryNames { get; set; } = new List<string>();
+        public IEnumerable<int> CategoryIds { get; set; } = new List<int>();
+        public IEnumerable<RestaurantViewModel> RestaurantViewModels { get; set; } = new List<RestaurantViewModel>();
+    }
+
 }
