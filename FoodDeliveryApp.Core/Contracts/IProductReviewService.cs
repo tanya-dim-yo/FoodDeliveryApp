543d2f97f2d3f9c06fcd1e112565fe769b4952cf
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-13 19:53:40 +0300

    ProductReviewService and contract added.

---
+++ FoodDeliveryApp.Core/Contracts/IProductReviewService.cs
@@ -0,0 +1,10 @@
+﻿using FoodDeliveryApp.Core.Models.ProductReview;
+
+namespace FoodDeliveryApp.Core.Contracts
+{
+    public interface IProductReviewService
+    {
+        Task AddProductReviewAsync(ProductReviewFormModel productReview);
+        Task<IEnumerable<ProductReviewViewModel>> GetProductReviewsByProductIdAsync(int productId);
+    }
+}
---
+++ FoodDeliveryApp.Core/Services/ProductReviewService.cs
@@ -0,0 +1,18 @@
+﻿using FoodDeliveryApp.Core.Contracts;
+using FoodDeliveryApp.Core.Models.ProductReview;
+
+namespace FoodDeliveryApp.Core.Services
+{
+    public class ProductReviewService : IProductReviewService
+    {
+        public Task AddProductReviewAsync(ProductReviewFormModel productReview)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProductReviewViewModel>> GetProductReviewsByProductIdAsync(int productId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
