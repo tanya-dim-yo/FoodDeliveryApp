98ce9e193d3062e09df81a9057093580085d9bed
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-13 20:37:18 +0300

    Migration to the database added - private setter of ItemReview removed and foreign key added.

---
+++ FoodDeliveryApp.Infrastructure/Migrations/20240413173609_ItemReviewSetter.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodDeliveryApp.Infrastructure.Migrations
+{
+    public partial class ItemReviewSetter : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ItemReviews_Items_ItemId",
+                table: "ItemReviews");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ItemId",
+                table: "ItemReviews",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ItemReviews_Items_ItemId",
+                table: "ItemReviews",
+                column: "ItemId",
+                principalTable: "Items",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ItemReviews_Items_ItemId",
+                table: "ItemReviews");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ItemId",
+                table: "ItemReviews",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ItemReviews_Items_ItemId",
+                table: "ItemReviews",
+                column: "ItemId",
+                principalTable: "Items",
+                principalColumn: "Id");
+        }
+    }
+}
