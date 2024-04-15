d6749f2d12bd2acaa74a7472b2a694d106a4ec10
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-16 00:39:41 +0300

    RoleConstants class created.

---
+++ FoodDeliveryApp.Core/Constants/RoleConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace FoodDeliveryApp.Core.Constants
+{
+    public static class RoleConstants
+    {
+        public const string AdminRole = "Administrator";
+        public const string AdminEmail = "admin@mail.com";
+    }
+}
