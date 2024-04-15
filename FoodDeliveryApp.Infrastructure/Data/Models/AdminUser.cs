41c0ad4420132b32531ddf4412eb6bce32333b26
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-16 00:06:27 +0300

    AdminUser entity created.

---
+++ FoodDeliveryApp.Infrastructure/Data/Models/AdminUser.cs
@@ -0,0 +1,8 @@
+﻿namespace FoodDeliveryApp.Infrastructure.Data.Models
+{
+    public class AdminUser
+    {
+        public const string AdminRoleName = "Administrator";
+        public const string AdminEmail = "admin@mail.com";
+    }
+}
