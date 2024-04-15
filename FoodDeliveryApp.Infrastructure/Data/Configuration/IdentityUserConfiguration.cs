d8cb5f15d2f0c96af0265abb00ac6bd627ea997c
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-15 09:55:29 +0300

    Seeding users configuration added.

---
+++ FoodDeliveryApp.Infrastructure/Data/Configuration/IdentityUserConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FoodDeliveryApp.Infrastructure.Data.Configuration
+{
+	public class IdentityUserConfiguration : IEntityTypeConfiguration<IdentityUser>
+	{
+		public void Configure(EntityTypeBuilder<IdentityUser> builder)
+		{
+			var data = new SeedData();
+
+			builder.HasData(new IdentityUser[] { data.GuestUser });
+		}
+	}
+}
---
+++ FoodDeliveryApp.Infrastructure/Data/Configuration/SeedData.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FoodDeliveryApp.Infrastructure.Data.Configuration
+{
+	public class SeedData
+	{
+		public IdentityUser GuestUser { get; set; }
+
+		public SeedData()
+		{
+			SeedUsers();
+		}
+
+		private void SeedUsers()
+		{
+			var hasher = new PasswordHasher<IdentityUser>();
+
+			GuestUser = new IdentityUser()
+			{
+				Id = "6d5800ce-d726-4fc8-83d9-d6b3ac1f591e",
+				UserName = "guest@mail.com",
+				NormalizedUserName = "guest@mail.com",
+				Email = "guest@mail.com",
+				NormalizedEmail = "guest@mail.com"
+			};
+
+			GuestUser.PasswordHash =
+			hasher.HashPassword(GuestUser, "guest123");
+		}
+	}
+}
