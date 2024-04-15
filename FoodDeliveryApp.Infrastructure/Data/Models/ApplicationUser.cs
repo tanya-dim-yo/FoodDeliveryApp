eb5e01002fcd9ee109c6bed1e6bc570e06e864ed
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-15 10:12:30 +0300

    ApplicationUser entity added.

---
+++ FoodDeliveryApp.Infrastructure/Data/Models/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using static FoodDeliveryApp.Infrastructure.Constants.ValidationConstants.ApplicationUserValidationConstants;
+
+namespace FoodDeliveryApp.Infrastructure.Data.Models
+{
+	public class ApplicationUser : IdentityUser
+	{
+		[Required]
+		[MaxLength(ApplicationUserFirstNameMaxLength)]
+		[PersonalData]
+		public string FirstName { get; set; } = string.Empty;
+
+		[Required]
+		[MaxLength(ApplicationUserLastNameMaxLength)]
+		[PersonalData]
+		public string LastName { get; set; } = string.Empty;
+	}
+}
