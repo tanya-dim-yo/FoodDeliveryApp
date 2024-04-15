b9b57509cafc9e132d45d9fd05d68cdc40622237
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-15 03:31:29 +0300

    Extension converting from Restaurant to RestaurantViewModel added.

---
+++ FoodDeliveryApp.Core/Extensions/IQuerableRestaurantExtension.cs
@@ -0,0 +1,23 @@
+﻿using FoodDeliveryApp.Core.Models.Restaurant;
+using FoodDeliveryApp.Infrastructure.Data.Models;
+
+namespace System.Linq
+{
+	public static class IQuerableRestaurantExtension
+	{
+		public static IQueryable<RestaurantViewModel> ProjectToRestaurantViewModel(this IQueryable<Restaurant> restaurants)
+		{
+			return restaurants
+				.Select(r => new RestaurantViewModel
+				{
+					Id = r.Id,
+					Title = r.Title,
+					ServiceFee = r.ServiceFee,
+					MinDeliveryTimeInMinutes = r.MinDeliveryTimeInMinutes,
+					MaxDeliveryTimeInMinutes = r.MaxDeliveryTimeInMinutes,
+					ImageURL = r.ImageURL,
+					RestaurantCategory = r.RestaurantCategory.Title
+				});
+		}
+	}
+}
