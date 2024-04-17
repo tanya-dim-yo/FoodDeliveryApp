d3d04215e6de7efe53443a541763c677ac09f103
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-17 10:41:54 +0300

    ShoppingCart contract created.

--- FoodDeliveryApp.Core/Contracts/IShoppingCartService.cs
+++ FoodDeliveryApp.Core/Contracts/IShoppingCartService.cs
@@ -1,9 +1,16 @@
-﻿namespace FoodDeliveryApp.Core.Contracts
+﻿using FoodDeliveryApp.Core.Models.ShoppingCart;
+
+namespace FoodDeliveryApp.Core.Contracts
 {
 	public interface IShoppingCartService
 	{
-		Task AddProductToCartAsync(int productId, int quantity);
-		Task RemoveProductFromCartAsync(int productId);
-		Task<decimal> CalculateTotalPriceAsync();
+		Task<bool> ExistsCartAsync(int cartId);
+		Task AddItemToCartAsync(int itemId, int quantity, int cartId);
+		Task AddAddOnToCartAsync(int addOnId, int quantity, int cartId);
+		Task RemoveItemFromCartAsync(int itemId, int quantity, int cartId);
+		Task RemoveAddOnFromCartAsync(int addOnId, int quantity, int cartId);
+		Task<IEnumerable<RecommendedItemViewModel>> GetRecommendedItemsAsync();
+		Task CalculateServiceFeeAsync(int cartId);
+		Task<decimal> CalculateTotalPriceAsync(int cartId);
 	}
 }
