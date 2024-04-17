6bbe20d70e0de9187a77a8bb03ff23e6ea5e8c4c
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-17 10:43:35 +0300

    AddItemToCart task added to the controller.

--- FoodDeliveryApp/Controllers/ShoppingCartController.cs
+++ FoodDeliveryApp/Controllers/ShoppingCartController.cs
@@ -1,14 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FoodDeliveryApp.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FoodDeliveryApp.Controllers
 {
 	public class ShoppingCartController : BaseController
 	{
-		public IActionResult Index()
-		{
-			
+		private readonly IShoppingCartService shoppingCartService;
 
-			return View();
+		public ShoppingCartController(IShoppingCartService _shoppingCartService)
+		{
+			shoppingCartService = _shoppingCartService;
 		}
+
+		[HttpPost]
+		public async Task<IActionResult> AddItemToCart(int itemId, int quantity, int cartId)
+		{
+			try
+			{
+				await shoppingCartService.AddItemToCartAsync(itemId, quantity, cartId);
+				return Ok("Продуктът е добавен успешно към количката!");
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, $"Възникна грешка: {ex.Message}");
+			}
+		}
+
+		
 	}
 }
