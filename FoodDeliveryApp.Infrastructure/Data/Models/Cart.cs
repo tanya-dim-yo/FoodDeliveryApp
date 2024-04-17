ec7c4578763de8becb1d3b16b1e949c360f64fe3
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-17 10:30:34 +0300

    Cart entity updated.

--- FoodDeliveryApp.Infrastructure/Data/Models/Cart.cs
+++ FoodDeliveryApp.Infrastructure/Data/Models/Cart.cs
@@ -1,10 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FoodDeliveryApp.Infrastructure.Data.Models
 {
-    public class Cart
+	public class Cart
     {
         [Key]
         public int Id { get; set; }
