c7465b2640020593c5a3980492f00f6579d182f8
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-17 13:09:09 +0300

    Shop cart models added.

---
+++ FoodDeliveryApp.Core/Models/ShoppingCart/ShoppingCartViewModel.cs
@@ -0,0 +1,21 @@
+﻿using FoodDeliveryApp.Infrastructure.Data.Models;
+
+namespace FoodDeliveryApp.Core.Models.ShoppingCart
+{
+	public class ShoppingCartViewModel
+	{
+		public int Id { get; set; }
+
+		public string UserId { get; set; } = string.Empty;
+
+		public decimal ItemsTotalPrice { get; set; }
+
+		public decimal ServiceFee { get; set; }
+
+		public decimal TotalPrice { get; set; }
+
+		public virtual IEnumerable<CartItemViewModel> CartItems { get; set; } = new List<CartItemViewModel>();
+
+		public virtual IEnumerable<RecommendedItemViewModel> RecommendedItems { get; set; } = new List<RecommendedItemViewModel>();
+	}
+}
