4faa2628ae760152056405b50e93829836f24256
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-18 12:08:10 +0300

    Blog articles models added.

--- FoodDeliveryApp.Core/Models/Blog/ArticlesWithCategoriesViewModel.cs
+++ FoodDeliveryApp.Core/Models/Blog/ArticlesWithCategoriesViewModel.cs
@@ -1,10 +1,9 @@
-﻿namespace FoodDeliveryApp.Core.Models.Restaurant
+﻿namespace FoodDeliveryApp.Core.Models.Blog
 {
     public class ArticlesWithCategoriesViewModel
     {
         public IEnumerable<string> CategoryNames { get; set; } = new List<string>();
         public IEnumerable<int> CategoryIds { get; set; } = new List<int>();
-        public IEnumerable<RestaurantViewModel> RestaurantViewModels { get; set; } = new List<RestaurantViewModel>();
+        public IEnumerable<BlogArticleViewModel> Articles { get; set; } = new List<BlogArticleViewModel>();
     }
-
 }
--- FoodDeliveryApp.Core/Models/Blog/BlogArticleViewModel.cs
+++ FoodDeliveryApp.Core/Models/Blog/BlogArticleViewModel.cs
@@ -2,5 +2,10 @@
 {
 	public class BlogArticleViewModel
 	{
+		public int Id { get; set; }
+		public string Title { get; set; } = string.Empty;
+		public string ImageUrl { get; set; } = string.Empty;
+		public DateTime PublicationDate { get; set; }
+		public string Resume { get; set; } = string.Empty;
 	}
 }
