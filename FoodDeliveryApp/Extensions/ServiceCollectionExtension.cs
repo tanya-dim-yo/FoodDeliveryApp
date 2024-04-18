1411fac239d45c4111fb0c3c34934968bb3c6fce
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-18 12:32:32 +0300

    BlogService added to the IoC container.

--- FoodDeliveryApp/Extensions/ServiceCollectionExtension.cs
+++ FoodDeliveryApp/Extensions/ServiceCollectionExtension.cs
@@ -16,6 +16,7 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddScoped<IRestaurantService, RestaurantService>();
             services.AddScoped<IProductService, ProductService>();
 			services.AddScoped<ICartService, CartService>();
+			services.AddScoped<IBlogService, BlogService>();
 
 			return services;
         }
