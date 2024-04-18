9ad873e37f1a021ff65e8f6428731a58b4a08066
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-18 12:09:38 +0300

    Extension projecting to BlogArticleViewModel added.

---
+++ FoodDeliveryApp.Core/Extensions/IQuerableBlogArticleExtension.cs
@@ -0,0 +1,21 @@
+﻿using FoodDeliveryApp.Core.Models.Blog;
+using FoodDeliveryApp.Infrastructure.Data.Models;
+
+namespace System.Linq
+{
+	public static class IQuerableBlogArticleExtension
+	{
+		public static IQueryable<BlogArticleViewModel> ProjectToBlogArticleViewModel(this IQueryable<BlogArticle> articles)
+		{
+			return articles
+				.Select(a => new BlogArticleViewModel
+				{
+					Id = a.Id,
+					Title = a.Title,
+					ImageUrl = a.Image,
+					PublicationDate = a.PublicationDate,
+					Resume = a.Content.Substring(0, 90)
+				});
+		}
+	}
+}
