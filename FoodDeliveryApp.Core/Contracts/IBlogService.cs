73af225a188cc4675782a2205f6ad268ec8a2757
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-18 12:11:06 +0300

    All articles and categories tasks added to the service.

--- FoodDeliveryApp.Core/Contracts/IBlogService.cs
+++ FoodDeliveryApp.Core/Contracts/IBlogService.cs
@@ -7,7 +7,8 @@ namespace FoodDeliveryApp.Core.Contracts
 	{
 		Task<bool> ExistsBlogCategoryAsync(int categoryId);
 		Task<bool> ExistsBlogArticleAsync(int articleId);
-		Task<IEnumerable<BlogArticleViewModel>> ArticlesListAsync();
+		Task<IEnumerable<BlogArticleViewModel>> AllArticlesAsync();
+		Task<IEnumerable<(int Id, string Title)>> AllBlogCategoriesAsync();
 		Task<IEnumerable<BlogArticleViewModel>> ArticlesByCategoryAsync(int categoryId);
 		Task<IEnumerable<string>> AllBlogCategoriesNamesAsync();
 		Task<(IEnumerable<BlogArticleViewModel> Articles, IEnumerable<(int Id, string Title)> Categories)> GetAllArticlesAndCategoriesAsync();
--- FoodDeliveryApp.Core/Services/BlogService.cs
+++ FoodDeliveryApp.Core/Services/BlogService.cs
@@ -1,10 +1,25 @@
 ﻿using FoodDeliveryApp.Core.Contracts;
 using FoodDeliveryApp.Core.Models.Blog;
+using FoodDeliveryApp.Infrastructure.Data.Common;
+using FoodDeliveryApp.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace FoodDeliveryApp.Core.Services
 {
 	public class BlogService : IBlogService
 	{
+		private readonly IRepository repository;
+		private readonly ILogger<BlogService> logger;
+
+		public BlogService(
+			IRepository _repository,
+			ILogger<BlogService> _logger)
+		{
+			repository = _repository;
+			logger = _logger;
+		}
+
 		public Task<IEnumerable<string>> AllBlogCategoriesNamesAsync()
 		{
 			throw new NotImplementedException();
@@ -15,7 +30,7 @@ namespace FoodDeliveryApp.Core.Services
 			throw new NotImplementedException();
 		}
 
-		public Task<IEnumerable<BlogArticleViewModel>> ArticlesListAsync()
+		public async Task<IEnumerable<BlogArticleViewModel>> AllArticlesAsync()
 		{
 			throw new NotImplementedException();
 		}
@@ -30,9 +45,31 @@ namespace FoodDeliveryApp.Core.Services
 			throw new NotImplementedException();
 		}
 
-		public Task<(IEnumerable<BlogArticleViewModel> Articles, IEnumerable<(int Id, string Title)> Categories)> GetAllArticlesAndCategoriesAsync()
+		public async Task<(IEnumerable<BlogArticleViewModel> Articles, IEnumerable<(int Id, string Title)> Categories)> GetAllArticlesAndCategoriesAsync()
 		{
-			throw new NotImplementedException();
+			var model = new ArticlesWithCategoriesViewModel();
+
+			var categories = await AllBlogCategoriesAsync();
+
+			var articles = await repository
+				.AllReadOnly<BlogArticle>()
+				.ProjectToBlogArticleViewModel()
+				.ToListAsync();
+
+			model.CategoryNames = categories.Select(c => c.Title);
+			model.CategoryIds = categories.Select(c => c.Id);
+
+			return (articles, categories.Select(c => (c.Id, c.Title)));
+		}
+
+		public async Task<IEnumerable<(int Id, string Title)>> AllBlogCategoriesAsync()
+		{
+			var categories = await repository
+				.AllReadOnly<BlogArticleCategory>()
+				.Select(c => new { c.Id, c.Title })
+				.ToListAsync();
+
+			return categories.Select(c => (c.Id, c.Title));
 		}
 	}
 }
