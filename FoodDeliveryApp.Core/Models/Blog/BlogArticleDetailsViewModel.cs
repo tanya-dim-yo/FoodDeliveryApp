d858069cf16f3d76c4a64eef528f0a7537ea6651
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-19 11:25:57 +0300

    Model updated with CategoryId and social networks string properties.

--- FoodDeliveryApp.Core/Models/Blog/BlogArticleDetailsViewModel.cs
+++ FoodDeliveryApp.Core/Models/Blog/BlogArticleDetailsViewModel.cs
@@ -16,6 +16,14 @@ namespace FoodDeliveryApp.Core.Models.Blog
 
 		public string Image { get; set; } = string.Empty;
 
+		public int CategoryId { get; set; }
+
 		public string Category { get; set; } = string.Empty;
+
+		public string FacebookUrl { get; set; } = string.Empty;
+
+		public string TwitterUrl { get; set; } = string.Empty;
+
+		public string LinkedInUrl { get; set; } = string.Empty;
 	}
 }
