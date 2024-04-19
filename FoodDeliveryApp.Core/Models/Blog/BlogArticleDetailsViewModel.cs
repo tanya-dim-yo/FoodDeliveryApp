422f73f9608a3b5591f7db4fe5ac8f3c57473330
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-19 10:11:07 +0300

    BlogArticleDetailsViewModel added.

--- FoodDeliveryApp.Core/Models/Blog/BlogArticleDetailsViewModel.cs
+++ FoodDeliveryApp.Core/Models/Blog/BlogArticleDetailsViewModel.cs
@@ -1,6 +1,21 @@
-﻿namespace FoodDeliveryApp.Core.Models.Blog
+﻿using FoodDeliveryApp.Infrastructure.Data.Models;
+
+namespace FoodDeliveryApp.Core.Models.Blog
 {
 	public class BlogArticleDetailsViewModel
 	{
+		public int Id { get; set; }
+
+		public string Title { get; set; } = string.Empty;
+
+		public string PublicationDate { get; set; } = string.Empty;
+
+		public int ReadingTime { get; set; }
+
+		public string Content { get; set; } = string.Empty;
+
+		public string Image { get; set; } = string.Empty;
+
+		public string Category { get; set; } = string.Empty;
 	}
 }
