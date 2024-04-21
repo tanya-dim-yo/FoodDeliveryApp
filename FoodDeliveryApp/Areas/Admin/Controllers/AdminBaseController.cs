b98ff166eaafd3fcff8c2f0fd1f6b07ff7a1bf4f
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-22 01:37:12 +0300

    Admin controllers added.

---
+++ FoodDeliveryApp/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static FoodDeliveryApp.Core.Constants.RoleConstants;
+
+namespace FoodDeliveryApp.Areas.Admin.Controllers
+{
+	[Authorize(Roles = AdminRole)]
+	public class AdminBaseController : Controller
+	{
+
+	}
+}
