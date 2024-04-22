1ab83e0ac4ae92d874b209588ea2611f6bdc4231
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-22 15:07:22 +0300

    GetUserName and GetUserEmail claims added.

--- FoodDeliveryApp/Extensions/ClaimsPrincipalExtension.cs
+++ FoodDeliveryApp/Extensions/ClaimsPrincipalExtension.cs
@@ -9,9 +9,20 @@ namespace System.Security.Claims
 			return user.FindFirstValue(ClaimTypes.NameIdentifier);
 		}
 
-        public static bool IsAdmin(this ClaimsPrincipal user)
-        {
-            return user.IsInRole(AdminRole);
-        }
-    }
+		public static string GetUserEmail(this ClaimsPrincipal user)
+		{
+			return user.FindFirst(ClaimTypes.Email)?.Value;
+		}
+
+		public static string GetUserName(this ClaimsPrincipal user)
+		{
+			return user.FindFirstValue(ClaimTypes.Name);
+		}
+
+		public static bool IsAdmin(this ClaimsPrincipal user)
+		{
+			return user.IsInRole(AdminRole);
+		}
+	}
+
 }
