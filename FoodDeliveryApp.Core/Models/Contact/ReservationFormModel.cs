c25c608298deffe30a053ae33dad764c87ba0366
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-22 15:06:26 +0300

    Reservation models added.

---
+++ FoodDeliveryApp.Core/Models/Contact/ReservationFormModel.cs
@@ -0,0 +1,15 @@
+﻿using FoodDeliveryApp.Core.Models.Restaurant;
+
+namespace FoodDeliveryApp.Core.Models.Contact
+{
+    public class ReservationFormModel
+	{
+		public string UserId { get; set; } = string.Empty;
+		public string Name { get; set; } = string.Empty;
+		public string Email { get; set; } = string.Empty;
+		public int NumberOfPeople { get; set; }
+		public string Message { get; set; } = string.Empty;
+		public int RestaurantID { get; set; }
+		public virtual IEnumerable<RestaurantReservationViewModel> Restaurants { get; set; } = new List<RestaurantReservationViewModel>();
+	}
+}
---
+++ FoodDeliveryApp.Core/Models/Restaurant/RestaurantReservationViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FoodDeliveryApp.Core.Models.Restaurant
+{
+    public class RestaurantReservationViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+    }
+}
