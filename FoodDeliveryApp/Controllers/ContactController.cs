250e784ddb80e1e240e16ef987c09b4db0b0da57
tanq.stoqnova@gmail.com <tanq.stoqnova@gmail.com>
2024-04-22 11:23:06 +0300

    ContactController and index page created.

---
+++ FoodDeliveryApp/Controllers/ContactController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodDeliveryApp.Controllers
+{
+	public class ContactController : BaseController
+	{
+		[AllowAnonymous]
+		public IActionResult ContactUs()
+		{
+			return View();
+		}
+	}
+}
